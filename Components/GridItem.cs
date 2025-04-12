bcae3fa8e7eb89d46f1c1a5f89aace640cd40adc
IIFabixn <f.becker33@outlook.de>
2025-04-12 16:17:14 +0200

    Add initial components: Card, FlexLayout, Grid, and GridItem

---
+++ Components/GridItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components;
+
+namespace RTB.BlazorUI.Components
+{
+    /// <summary>
+    /// Helper Component to register itself in the parent Grid.
+    /// </summary>
+    public class GridItem : ComponentBase
+    {
+        [CascadingParameter] public Grid Parent { get; set; } = null!;
+        [Parameter] public int Column { get; set; } = 1;
+        [Parameter] public int ColumnSpan { get; set; } = 1;
+        [Parameter] public int Row { get; set; } = 1;
+        [Parameter] public int RowSpan { get; set; } = 1;
+        [Parameter] public RenderFragment ChildContent { get; set; } = (builder) => { };
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object?> CapturesAttributes { get; set; } = new();
+
+        protected override void OnParametersSet()
+        {
+            Parent.RegisterItem(this);
+        }
+
+        public void Dispose()
+        {
+            Parent.UnregisterItem(this);
+        }
+    }
+}
