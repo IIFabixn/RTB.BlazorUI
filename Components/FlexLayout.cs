bc8b80685507884c0ab1edf461914e633609bde7
IIFabixn <f.becker33@outlook.de>
2025-04-12 23:09:19 +0200

    Refactor CollectionList and FlexLayout components; remove FlexLayout.razor and implement FlexLayout.cs

---
+++ Components/FlexLayout.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace RTB.BlazorUI.Components;
+public class FlexLayout : RTBComponent
+{
+    [Parameter] public bool IsHorizontal { get; set; } = false;
+
+    [Parameter] public bool IsVertical { get; set; } = false;
+    [Parameter] public bool FullHeight { get; set; } = false;
+
+    [Parameter] public string Element { get; set; } = "div";
+
+    [Parameter] public RenderFragment ChildContent { get; set; } = default!;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, Element);
+        builder.AddMultipleAttributes(1, CapturedAttributes.Where(kvp => kvp.Key != "class"));
+        builder.AddAttribute(2, "class", $"flex {(FullHeight ? "h-full" : string.Empty)} {(IsHorizontal ? "flex-row" : string.Empty)} {(IsVertical ? "flex-col" : string.Empty)} {(CapturedAttributes.TryGetValue("class", out var classes) ? classes : string.Empty)}");
+        builder.AddContent(3, ChildContent);
+        builder.CloseElement();
+    }
+}
