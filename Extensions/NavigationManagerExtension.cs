19f2f190b3bb7113424341ae7c238b9fb4d10d7a
Fabian Becker <f.becker33@outlook.de>
2025-04-18 00:23:27 +0200

    Fix inheritance in DataGrid component and add NavigationManager extension for URI parameter handling

---
+++ Extensions/NavigationManagerExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace RTB.BlazorUI.Extensions
+{
+    public static class NavigationManagerExtension
+    {
+        public static TValue? GetUriParams<TValue>(this NavigationManager navigationManager, string key)
+        {
+            var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+            var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            var jsonValue = query[key];
+            if (string.IsNullOrEmpty(jsonValue))
+                return default;
+
+            try
+            {
+                return JsonSerializer.Deserialize<TValue>(jsonValue);
+            }
+            catch (JsonException)
+            {
+                return default;
+            }
+        }
+
+        public static void NavigateWithParams(this NavigationManager navigationManager, string uri, params KeyValuePair<string, object>[] parameters)
+        {
+            var uriBuilder = new UriBuilder(navigationManager.ToAbsoluteUri(uri));
+            var query = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+            foreach (var parameter in parameters)
+            {
+                query[parameter.Key] = JsonSerializer.Serialize(parameter.Value);
+            }
+            uriBuilder.Query = query.ToString();
+            navigationManager.NavigateTo(uriBuilder.ToString());
+        }
+    }
+}
