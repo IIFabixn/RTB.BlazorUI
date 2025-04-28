7e2cf27dbbd991105988d47faf262b1c57164b20
Fabian Becker <fabian.becker@lr.org>
2025-04-28 13:20:36 +0200

    - Updated project files for multiple components to target .NET 9.0, including new package references and configurations. - Added new project references for `HS.CPS.Shared` and other modules to improve modularity. - Modified several Razor components to include new using directives and layout changes for better organization. - Restructured `MainLayout.razor` and `SideMenuLayout.razor` for enhanced layout management. - Introduced new classes and attributes for improved component organization and parameter handling. - Added or modified JavaScript interop files to support new functionalities. - Updated the solution file to include new projects and configurations for proper referencing. - Enhanced styling and layout capabilities in `Grid.razor` and `Paper.razor`. - Refactored `MemoryCache.cs` to change the namespace and improve caching implementation. - Added `background.png` with versioning information tracked by Git LFS.

---
+++ AttachedParameter/AttachedParameter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTB.BlazorUI.AttachedParameter
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class AttachedParameterAttribute : Attribute
+    {
+    }
+}
---
+++ AttachedParameter/AttachedParameterActivator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTB.BlazorUI.AttachedParameter
+{
+    public class AttachedParameterACtivator(IServiceProvider serviceProvider) : IComponentActivator
+    {
+        private static readonly ConcurrentDictionary<Type, ObjectFactory> _cachedComponentTypeInfo = new();
+
+        public static void ClearCache() => _cachedComponentTypeInfo.Clear();
+
+        private readonly Dictionary<Type, IEnumerable<PropertyInfo>> attachedParametersCache = [];
+        public IComponent CreateInstance([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type componentType)
+        {
+            if (!typeof(IComponent).IsAssignableFrom(componentType))
+            {
+                throw new ArgumentException($"The type {componentType.FullName} does not implement {nameof(IComponent)}.", nameof(componentType));
+            }
+
+            if (!attachedParametersCache.TryGetValue(componentType, out var namedParameters))
+            {
+                namedParameters = componentType.GetProperties().Where(p => p.GetCustomAttributes<AttachedParameterAttribute>() != null);
+                attachedParametersCache[componentType] = namedParameters;
+            }
+
+            var factory = GetObjectFactory(componentType);
+
+            return (IComponent)factory(serviceProvider, []);
+        }
+        private static ObjectFactory GetObjectFactory([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type componentType)
+        {
+            // Unfortunately we can't use 'GetOrAdd' here because the DynamicallyAccessedMembers annotation doesn't flow through to the
+            // callback, so it becomes an IL2111 warning. The following is equivalent and thread-safe because it's a ConcurrentDictionary
+            // and it doesn't matter if we build a cache entry more than once.
+            if (!_cachedComponentTypeInfo.TryGetValue(componentType, out var factory))
+            {
+                factory = ActivatorUtilities.CreateFactory(componentType, Type.EmptyTypes);
+                _cachedComponentTypeInfo.TryAdd(componentType, factory);
+            }
+
+            return factory;
+        }
+    }
+}
