8522b13fb5410304f91b371760eee248cc67e720
IIFabixn <f.becker33@outlook.de>
2025-07-05 13:13:57 +0200

    Enhance layout and add charts functionality
    - Updated `MainLayout.razor` for improved structure.
    - Added new `Charts.razor` page with `LineChart` component.
    - Modified `Home.razor` to include navigation to charts.
    - Changed environment variable in `Program.cs` to "Debug".
    - Updated `_Imports.razor` with new namespaces.
    - Adjusted `app.css` for layout dimensions.
    - Enhanced SVG handling in `Icon.razor` using `HexRgba`.
    - Renamed method in `Select.razor` for consistency.
    - Incremented version in `RTB.Blazor.UI.csproj` to 1.0.3.
    - Updated color handling in `BarChart.razor` and `DounutChart.razor` to use `HexRgba`.
    - Changed `ChartEntry` in `ChartBase.cs` to use `RTBColor`.
    - Updated `LineChart.razor` to use `RTBColor` for line colors.
    - Incremented version in `RTB.Blazor.Charts.csproj` to 1.0.4.

--- src/RTB.Charts/Components/ChartBase.cs
+++ src/RTB.Charts/Components/ChartBase.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
 using RTB.Blazor.Core;
+using RTB.Blazor.Styled.Helper;
 using SkiaSharp;
 
 namespace RTB.Blazor.Charts.Components
 {
     public abstract class ChartBase : RTBComponent
     {
-        public static readonly SKColor ACTIVE = 0xFF22C55E; // emerald-500
-        public static readonly SKColor WAITING = 0xFFF59E0B; // amber-500
-        public static readonly SKColor COMPLETED = 0xFF0EA5E9; // sky-500
-        public static readonly SKColor CLOSED = 0xFF6366F1; // indigo-500
-
-        public static readonly SKColor REPAIR = 0xFF6366F1;
-        public static readonly SKColor CLEANING = 0xFF0EA5E9;
-        public static readonly SKColor HEATING = 0xFFF97316;
-
         [Parameter, EditorRequired] public IList<ChartEntry> Entries { get; set; } = [];
         public float MaxValue => Entries.Max(e => e.Value);
         public float MinValue => Entries.Min(e => e.Value);
@@ -28,5 +20,13 @@ namespace RTB.Blazor.Charts.Components
         }
     }
 
-    public record ChartEntry(string Label, float Value, SKColor? Color = null);
+    public record ChartEntry(string Label, float Value, RTBColor Color);
+
+    internal static class ColorExtension
+    {
+        internal static SKColor ToSKColor(this RTBColor color)
+        {
+            return new SKColor(color.R, color.G, color.B, color.A);
+        }
+    }
 }
